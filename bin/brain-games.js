4225a9bc6aba98088bd905e5a2d2a17666aed966
IULIIA IAKUTOVA <julia@IULIIAs-MacBook-Pro.local>
2020-06-25 15:45:54 +0300

    Ask user name and greet.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!')
+
+import { askNameAndGreet } from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+
+askNameAndGreet();
---
+++ src/cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import readlineSynk from 'readline-sync';
+
+const askNameAndGreet = () => {
+  const name = readlineSynk.question('May I have your name? ');
+
+  console.log(`Hello, ${name}!`);
+}
+
+export { askNameAndGreet };
