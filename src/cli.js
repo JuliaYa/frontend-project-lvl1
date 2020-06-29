4036f142cf1118d4119552abd1e55ce2301a8bb1
Julia Yakutova <julia.yakutova@gmail.com>
2020-06-29 23:53:24 +0300

    Test. Make Eslint to find problems.

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@ import readlineSynk from 'readline-sync';
 
 const askNameAndGreet = () => {
   const name = readlineSynk.question('May I have your name? ');
-
+  var a = 2
   console.log(`Hello, ${name}!`);
 }
 
