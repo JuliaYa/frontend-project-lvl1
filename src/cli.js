75cda5ff3268cf4db73fec98d821412a11209cc3
Julia Yakutova <julia.yakutova@gmail.com>
2020-06-29 23:59:12 +0300

    Fix test problems.

--- src/cli.js
+++ src/cli.js
@@ -4,8 +4,8 @@ import readlineSynk from 'readline-sync';
 
 const askNameAndGreet = () => {
   const name = readlineSynk.question('May I have your name? ');
-  var a = 2
+
   console.log(`Hello, ${name}!`);
-}
+};
 
 export { askNameAndGreet };
