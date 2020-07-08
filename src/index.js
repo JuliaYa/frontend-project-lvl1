8cc3a11d9faede2861d7bf8a2db06ac81a99796e
Julia Yakutova <julia.yakutova@gmail.com>
2020-07-08 16:28:21 +0300

    Refactoring.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 import readlineSynk from 'readline-sync';
 
 const askNameAndGreet = () => {
@@ -10,12 +8,12 @@ const askNameAndGreet = () => {
   return name;
 };
 
-function rand(maxLimit = 100) {
-  let rand = Math.random() * maxLimit;
-  return Math.floor(rand);
- }
+const rand = (maxLimit = 100) => {
+  const randomNumber = Math.random() * maxLimit;
+  return Math.floor(randomNumber);
+};
 
- const startGame = (params) => {
+const startGame = (params) => {
   console.log('Welcome to the Brain Games!');
 
   const name = readlineSynk.question('May I have your name? ');
@@ -27,7 +25,7 @@ function rand(maxLimit = 100) {
   let counter = 0;
   let exit = false;
 
-  const {questions, answers} = params;
+  const { questions, answers } = params;
 
   while (!exit) {
     console.log(`Question: ${questions[counter]}`);
@@ -36,19 +34,19 @@ function rand(maxLimit = 100) {
     const trueAnswer = answers[counter];
 
     if (answer.toLocaleLowerCase() === trueAnswer) {
-        console.log('Correct!');
-        counter += 1;
-        exit = counter === 3;
+      console.log('Correct!');
+      counter += 1;
+      exit = counter === 3;
     } else {
-        exit = true;
+      exit = true;
 
-        console.log(`"${answer}" is wrong answer ;(. Correct answer was "${trueAnswer}".`);
-        console.log(`Let's try again, ${name}!`);
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${trueAnswer}".`);
+      console.log(`Let's try again, ${name}!`);
 
-        return;
+      return;
     }
   }
-    console.log(`Congratulations, ${name}!`);
- };
+  console.log(`Congratulations, ${name}!`);
+};
 
 export { askNameAndGreet, rand, startGame };
