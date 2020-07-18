462239d9f423b3fd509860826ed90f3586df14ac
Julia Yakutova <julia.yakutova@gmail.com>
2020-07-18 17:33:21 +0300

    Refactoring. Rewrite GCD game.

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,6 +1,6 @@
 import { rand, startGame } from '../index.js';
 
-const findGCD = ([num1, num2]) => {
+const findGCD = (num1, num2) => {
   let temp;
   let a = num1;
   let b = num2;
@@ -13,19 +13,19 @@ const findGCD = ([num1, num2]) => {
   return a;
 };
 
-export default () => {
-  const q1 = `${rand(100)} ${rand(100)}`;
-  const q2 = `${rand(100)} ${rand(100)}`;
-  const q3 = `${rand(100)} ${rand(100)}`;
+const getChallenge = () => {
+  const number1 = rand(100);
+  const number2 = rand(100);
 
-  const a1 = findGCD(q1.split(' ')).toString();
-  const a2 = findGCD(q2.split(' ')).toString();
-  const a3 = findGCD(q3.split(' ')).toString();
+  return [`${number1} ${number2}`, findGCD(number1, number2).toString()];
+};
+
+export default () => {
+  const challenges = [getChallenge(), getChallenge(), getChallenge()];
 
   const gameParams = {
     description: 'Find the greatest common divisor of given numbers.',
-    questions: [q1, q2, q3],
-    answers: [a1, a2, a3],
+    challenges,
   };
 
   startGame(gameParams);
