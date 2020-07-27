35bb410aff4d27c66d54df5c455babe29b7fea02
Julia Yakutova <julia.yakutova@gmail.com>
2020-07-27 19:12:26 +0300

    Refactoring. Progression game.

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,20 +1,24 @@
 import startGame, { getNumberOfRounds } from '../index.js';
 import { generateNumber } from '../utils.js';
 
-const getProgression = (start, interval, count = 10) => {
-  const progression = [start];
+const progressionLength = 10;
 
-  for (let i = 1; i < count; i += 1) {
-    const nextElement = progression[i - 1] + interval;
-    progression.push(nextElement);
+const getProgression = () => {
+  const progression = [];
+  const step = generateNumber();
+
+  progression.push(generateNumber());
+  for (let i = 1; i < progressionLength; i += 1) {
+    progression.push(progression[i - 1] + step);
   }
   return progression;
 };
 
 const generateRound = () => {
-  const progressionSize = 10;
-  const progression = getProgression(generateNumber(), generateNumber(), progressionSize);
-  const hiddenIndex = generateNumber(0, progressionSize);
+  /* мне кажется что скрытый индекс в генерацию прогрессии добавлять не стот */
+  const progression = getProgression();
+
+  const hiddenIndex = generateNumber(0, progressionLength);
   const answer = progression[hiddenIndex];
   progression[hiddenIndex] = '..';
 
@@ -24,7 +28,7 @@ const generateRound = () => {
 export default () => {
   const description = 'What number is missing in the progression?';
 
-  const rounds = Array.from(Array(getNumberOfRounds), () => generateRound());
+  const rounds = Array.from(Array(getNumberOfRounds()), () => generateRound());
 
   startGame(description, rounds);
 };
